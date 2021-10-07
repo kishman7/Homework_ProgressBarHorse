926a234d4905944012795c24a9791f6324cc7100
kishman7 <kishman_us@ukr.net>
2021-10-07 23:12:46 +0300

    Add Homework_ProgressBarHorse 2

--- Homework_ProgressBarHorse/MainWindow.xaml.cs
+++ Homework_ProgressBarHorse/MainWindow.xaml.cs
@@ -25,33 +25,45 @@ namespace Homework_ProgressBarHorse
         public MainWindow()
         {
             InitializeComponent();
-            Thread.Sleep(5000);
-
         }
 
         private void btnStart_Click(object sender, RoutedEventArgs e)
         {
-            Thread progressBarStart1 = new Thread(ProgressBarGo);
-            progressBarStart1.Start(pb1);
-            progressBarStart1.IsBackground = true;
-            Thread progressBarStart2 = new Thread(ProgressBarGo);
-            progressBarStart2.Start(pb2);
-            progressBarStart2.IsBackground = true;
-            Thread progressBarStart3 = new Thread(ProgressBarGo);
-            progressBarStart3.Start(pb3);
-            progressBarStart3.IsBackground = true;
-            Thread progressBarStart4 = new Thread(ProgressBarGo);
-            progressBarStart4.Start(pb4);
-            progressBarStart4.IsBackground = true; progressBarStart2.IsBackground = true;
-            Thread progressBarStart5 = new Thread(ProgressBarGo);
-            progressBarStart5.Start(pb5);
-            progressBarStart5.IsBackground = true;
-            //Thread.Sleep(3000);
-            foreach (Label label in stackPanel.Children)
+            foreach (var item in stackPanel.Children)
             {
-                listBox.Items.Add(label.Content);
-                //listBox.Items.Add((item as ListBox).Items);
+                if (item is ProgressBar)
+                {
+                    Thread progressBarStart1 = new Thread(ProgressBarGo);
+                    progressBarStart1.Start(item);
+                    progressBarStart1.IsBackground = true;
+                }
+
             }
+            //Thread progressBarStart1 = new Thread(ProgressBarGo);
+            //progressBarStart1.Start(pb1);
+            //progressBarStart1.IsBackground = true;
+            //Thread progressBarStart2 = new Thread(ProgressBarGo);
+            //progressBarStart2.Start(pb2);
+            //progressBarStart2.IsBackground = true;
+            //Thread progressBarStart3 = new Thread(ProgressBarGo);
+            //progressBarStart3.Start(pb3);
+            //progressBarStart3.IsBackground = true;
+            //Thread progressBarStart4 = new Thread(ProgressBarGo);
+            //progressBarStart4.Start(pb4);
+            //progressBarStart4.IsBackground = true; 
+            //Thread progressBarStart5 = new Thread(ProgressBarGo);
+            //progressBarStart5.Start(pb5);
+            //progressBarStart5.IsBackground = true;
+            //Thread.Sleep(3000);
+
+            //foreach (var label in stackPanel.Children)
+            //{
+            //    if (label is Label)
+            //    {
+            //        listBox.Items.Add((label as Label).Content);
+            //    }
+            //}
+
             //listBox.Items.Add(lbl1.Content);
             //listBox.Items.Add(lbl2.Content);
             //listBox.Items.Add(lbl3.Content);
@@ -63,13 +75,20 @@ namespace Homework_ProgressBarHorse
 
         void ProgressBarGo(object pb)
         {
+            Random random = new Random();
             var progressBar = (pb as ProgressBar);
-            progressBar.Dispatcher.Invoke(DispatcherPriority.Normal, (ThreadStart)delegate () {
-                for (int i = 0; i < progressBar.Maximum; i++)
+            //progressBar.Dispatcher.Invoke(DispatcherPriority.Normal, (ThreadStart)delegate () { });
+            for (int i = 0; i <= 100; i++)
                 {
                     progressBar.Dispatcher.Invoke(DispatcherPriority.Normal, (ThreadStart)delegate () { progressBar.Value = i; }); //кожному значенню progressBar присвоюємо значення i
+                    Thread.Sleep(random.Next(0,100));
                 }
-            }); //дістаємо з потоку progressBar та присвоюємо максимальному значенню довжину файла
+                progressBar.Dispatcher.Invoke(DispatcherPriority.Normal, (ThreadStart)delegate () {
+                listBox.Items.Add(progressBar.Tag);
+                });
+
+            //дістаємо з потоку progressBar та присвоюємо максимальному значенню довжину файла
+
         }
 
     }
